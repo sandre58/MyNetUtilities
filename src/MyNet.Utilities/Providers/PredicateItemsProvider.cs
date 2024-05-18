a3cae1eb8198f5908d832fad3c95573e9481923a
Stéphane ANDRE <andre.cs2i@gmail.com>
2024-05-18 11:01:22 +0200

    feat: Add an items provider with predicate

---
+++ src/MyNet.Utilities/Providers/PredicateItemsProvider.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyNet.Utilities.Providers
+{
+    public class PredicateItemsProvider<T> : IItemsProvider<T>
+    {
+        private readonly Func<T, bool> _predicate;
+        private readonly IItemsProvider<T> _itemsProvider;
+
+        public PredicateItemsProvider(IItemsProvider<T> provider, Func<T, bool> predicate)
+        {
+            _itemsProvider = provider;
+            _predicate = predicate;
+        }
+
+        public IEnumerable<T> ProvideItems() => _itemsProvider.ProvideItems().Where(_predicate);
+    }
+}
